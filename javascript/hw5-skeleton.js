ce94a567be6e052729dea5b24747640996ff332e
afrankl <afrankl@berkeley.edu>
2016-02-24 14:20:09 -0800

    Removed unnecessary files

--- javascript/hw5-skeleton.js
+++ javascript/hw5-skeleton.js
@@ -1,17 +1,35 @@
 // Event hander for calling the SoundCloud API using the user's search query
+var offset = 0;
+
 function callAPI(query) {
 	$.get("https://api.soundcloud.com/tracks?client_id=b3179c0738764e846066975c2571aebb",
 		{'q': query,
-		'limit': '200'},
+		'limit': '1',
+		'offset' : '' + offset + ''},
 		function(data) {
 			// PUT IN YOUR CODE HERE TO PROCESS THE SOUNDCLOUD API'S RESPONSE OBJECT
 			// HINT: CREATE A SEPARATE FUNCTION AND CALL IT HERE
+			handleData(data);
 		},'json'
 	);
+	offset += 1;
 }
 
 function search() {
-	callAPI($('#search-music').val());
+	var query = $('input','#search').val();
+	console.log(query);
+	callAPI(query);
+}
+
+function handleData(data) {
+	for (var i = 0; i < data.length; i++) {
+		var sc_obj = data[i];
+		console.log(sc_obj);
+	}
+}
+
+function createQueryTag(sc_obj) {
+
 }
 
 // 'Play' button event handler - play the track in the Stratus player
